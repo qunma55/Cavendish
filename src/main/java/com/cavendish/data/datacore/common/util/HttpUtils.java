17875313f1f13c681d627e0d9b5686a290e08f7c
hewei <hewei@xiniaoyun.com>
2019-11-22 16:13:08 +0800

    feature:新增guava库，新增访问高德地图行政区划接口demo

--- src/main/java/com/cavendish/data/datacore/common/util/HttpUtils.java
+++ src/main/java/com/cavendish/data/datacore/common/util/HttpUtils.java
@@ -1,18 +1,21 @@
 package com.cavendish.data.datacore.common.util;
 
+import com.google.common.io.CharStreams;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 
 /**
@@ -21,21 +24,24 @@ import java.util.Objects;
  * @author hevigo9
  */
 public class HttpUtils {
-  private final static Logger logger = LoggerFactory.getLogger(HttpUtils.class);
+  private static final Logger logger = LoggerFactory.getLogger(HttpUtils.class);
 
-  public static void main(String[] args) {
+  // 日志保留请求现场 请求地址，请求时间，请求结果，...
+  // 多个普通参数，FORM表单，JSON数据，XML数据
+  // GET POST
+
+  public static void main(String[] args) throws Exception {
     CloseableHttpClient httpClient = HttpClients.createDefault();
     HttpGet httpGet = null;
     try {
       URI uri =
           new URIBuilder()
-              .setScheme("http")
-              .setHost("www.google.com")
-              .setPath("/search")
-              .setParameter("q", "httpclient")
-              .setParameter("btnG", "Google Search")
-              .setParameter("aq", "f")
-              .setParameter("oq", "")
+              .setScheme("https")
+              .setHost("restapi.amap.com")
+              .setPath("/v3/config/district")
+              .setParameter("key", "ff42e94f707fb86646d59f8d72c46862")
+              .setParameter("keywords", "山东")
+              .setParameter("subdistrict", "2")
               .build();
       httpGet = new HttpGet(uri);
     } catch (URISyntaxException e) {
@@ -48,20 +54,12 @@ public class HttpUtils {
       }
       response = httpClient.execute(httpGet);
       // 响应体
-      HttpEntity responseEntity = response.getEntity();
-      System.out.println("响应状态：" + response.getStatusLine());
-      // gzip,deflate,compress
-      System.out.println("响应体编码方式：" + responseEntity.getContentEncoding());
-      // 响应类型如text/html charset也有可能在ContentType中
-      System.out.println("响应体类型：" + responseEntity.getContentType());
-      /*
-       EntityUtils.toString()方法会将响应体的输入流关闭，相当于消耗了响应体， 此时连接会回到httpclient中的连接管理器的连接池中，如果
-       下次访问的路由是一样的， 则此连接可以被复用。
-      */
-      System.out.println("响应体内容：" + EntityUtils.toString(responseEntity));
-      // 如果关闭了httpEntity的inputStream，httpEntity长度应该为0，而且再次请求相同路由的连接可以共用一个连接。
-      // 可以通过设置连接管理器最大连接为1来验证。
-      response = httpClient.execute(httpGet);
+      if (!Objects.isNull(response)) {
+        HttpEntity responseEntity = response.getEntity();
+        InputStream instream = responseEntity.getContent();
+        String str = CharStreams.toString(new InputStreamReader(instream, StandardCharsets.UTF_8));
+        System.out.println(str);
+      }
     } catch (IOException e) {
       e.printStackTrace();
     } finally {
@@ -76,8 +74,4 @@ public class HttpUtils {
       }
     }
   }
-
-  // 日志保留请求现场 请求地址，请求时间，请求结果，...
-  // 多个普通参数，FORM表单，JSON数据，XML数据
-  // GET POST
 }
