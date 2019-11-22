931e5c75e962483ebdea6e335d96ceccddac80d8
hewei <hewei@xiniaoyun.com>
2019-11-22 10:13:09 +0800

    first commit

---
+++ src/main/java/com/cavendish/data/datacore/DataCoreApplication.java
@@ -0,0 +1,13 @@
+package com.cavendish.data.datacore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cavendish/data/datacore/DataCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cavendish.data.datacore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
